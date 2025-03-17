dfdb6e3c01b864aad098d6b42038d6dceb913826
Patrik <Muravski.patrik@gmail.com>
2025-03-17 19:03:34 +0200

    page is added to view, controller, and layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace UnityWeb.Controllers
             return View();
         }
 
+        public IActionResult Unity()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
